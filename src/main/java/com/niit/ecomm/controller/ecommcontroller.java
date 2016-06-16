f03f656f2ef2f69bc782c60ef5b1e22410d2fc10
nimit pamnanni <nimit pamnanni>
2016-06-16 13:56:34 +0530

    adding files 2

---
+++ src/main/java/com/niit/ecomm/controller/ecommcontroller.java
@@ -0,0 +1,15 @@
+package com.niit.ecomm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ecommcontroller {
+
+	@RequestMapping("/")
+	public String helloecomm()
+	{
+		return "index";
+	}
+	
+}
